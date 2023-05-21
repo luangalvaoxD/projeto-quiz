081e9170113103ead9fea7bc3b57d31f049507a6
Luan Galvão <luanestudos98@gmail.com>
2023-05-20 22:37:45 -0300

    feat: add global styles and fonts

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+import './styles/global.css'
+
+function MeuNome () {
+  return (
+    <h2>Luan Cardoso</h2>
+  )
+}
+
+function App() {
+  return (
+    <div>
+    <h1>Olá galera!</h1>
+    <MeuNome/>
+    </div>
+    
+  )
+}
+
+export default App
